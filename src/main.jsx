ad5584bd9643f633e20c147fffa3f50f27c20d5f
omuneercc <omuneercc@gmail.com>
2026-07-31 02:02:29 +0500

    Add Tailwind CSS for styling

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 import "./lib/storageShim"; // sets up window.storage backed by Supabase, before anything renders
 import AppGate from "./AppGate";
 
